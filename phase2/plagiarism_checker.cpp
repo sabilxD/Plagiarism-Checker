14ba04547733bc89dff6d84ecc9fff6c52facce4
aditisingh1053 <23b1053@iitb.ac.in>
2024-11-19 02:29:11 +0530

    my first task !! yipeeee

--- phase2/plagiarism_checker.cpp
+++ phase2/plagiarism_checker.cpp
@@ -3,5 +3,49 @@
 // Do NOT add "using namespace std;".
 
 // TODO: Implement the methods of the plagiarism_checker_t class
+void exact_match(std::vector<int>& tokens, int & match_length,
+                 std::atomic<int>& count_matches, std::atomic<bool>& plagged,
+                 std::shared_ptr<submission_t>& sub1, std::shared_ptr<submission_t>& sub2){
+                 count_matches+=21;
 
+
+}
+
+void plagiarism_checker_t::add_submission(std::shared_ptr<submission_t> __submission) {
+
+    auto timestamp = std::chrono::steady_clock::now();
+    std::vector<int> tokens = tokenizer_t(__submission->codefile).get_tokens();
+    int matchlength=75;
+    std::atomic<int> count_matches(0);
+    std::atomic<bool> plagged(false);
+    std::vector<std::thread> threads;
+
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        for (auto &prev_submission : submissions) {
+            // Launch a thread for each previous submission comparison
+            threads.emplace_back(&plagiarism_checker_t::exact_match, this,
+                                 tokens, matchlength, count_matches, plagged,
+                                 __submission, prev_submission.second);
+        }
+
+        // Add the new submission to the list
+        submissions.push_back({timestamp,__submission});
+    }
+
+    // Join all threads to ensure all comparisons are complete
+    for (auto &t : threads) {
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+    // TO detect patched plagiarism
+    if (!plagged){
+        if (count_matches>=20){
+            //declare patched plag
+            __submission->student->flag_student(__submission);
+            __submission->professor->flag_professor(__submission);
+        }
+    }
+}
 // End TODO
--- phase2/plagiarism_checker.hpp
+++ phase2/plagiarism_checker.hpp
@@ -2,10 +2,15 @@
 // -----------------------------------------------------------------------------
 
 // You are free to add any STL includes above this comment, below the --line--.
+#include <chrono>
+#include <atomic>
+#include <mutex>
+#include <thread>
 // DO NOT add "using namespace std;" or include any other files/libraries.
 // Also DO NOT add the include "bits/stdc++.h"
 
 // OPTIONAL: Add your helper functions and classes here
+
 
 class plagiarism_checker_t {
     // You should NOT modify the public interface of this class.
@@ -18,6 +23,14 @@ public:
 
 protected:
     // TODO: Add members and function signatures here
-    
+    void exact_match(std::vector<int>& tokens, int & match_length,
+                     std::atomic<int>& count_matches, std::atomic<bool>& plagged,
+                     std::shared_ptr<submission_t>& sub1, std::shared_ptr<submission_t>& sub2);
+
+
+    std::vector<std::pair<std::chrono::time_point<std::chrono::steady_clock>,
+                        std::shared_ptr<submission_t>>> submissions;
+    std::mutex mtx;
+    std::thread worker_thread;
     // End TODO
 };
